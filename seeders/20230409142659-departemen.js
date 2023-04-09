8d50a941ee201d4d4a2cfee02ef42b9ce988cc3f
fakhryhizballah <falehry88@gmail.com>
2023-04-09 21:41:01 +0700

    seeder departement

---
+++ seeders/20230409142659-departemen.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    return queryInterface.bulkInsert("Departemen", [
+      {
+        bidang: "Dokter Spesialis",
+      },
+      {
+        bidang: "UMUM DAN KEPEGAWAIAN",
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete("Departemen", null, {});
+  },
+};
