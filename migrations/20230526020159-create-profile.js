a688ce791a666047a0015c5fadf2589dcdcff224
fakhryhizballah <falehry88@gmail.com>
2023-05-26 09:38:23 +0700

    foto profie

---
+++ migrations/20230526020159-create-profile.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nik: {
+        type: Sequelize.BIGINT(16),
+        references: {
+          model: {
+            tableName: 'Users'
+          },
+          key: 'nik',
+        }
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
