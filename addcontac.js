3e04b51fbb13f51c4690bf32e32c0fba70236308
fakhryhizballah <falehry88@gmail.com>
2023-05-28 18:48:16 +0700

    add contac

---
+++ addcontac.js
@@ -0,0 +1,63 @@
+const { User, Profile } = require("./models");
+const { Op } = require("sequelize");
+const axios = require('axios');
+const { use } = require("express/lib/application");
+
+async function getUser(){
+  let user = await User.findAll({
+    limit: 10,
+    offset: 110,
+    order: [["id", "ASC"]],
+  });
+  console.log(user.length);
+  let count = 0;
+  for (let i = 0; i < user.length; i++) {
+    let profile = await Profile.findOne({
+      where: {
+        nik: user[i].nik,
+      },
+    });
+    if (!profile) {
+      count++;
+      saveContak(user[i]);
+    }
+  }
+  console.log(count);
+
+  return user;
+}
+getUser();
+async function saveContak(user){
+  let tgl = user.tgl_lahir.split("-");
+  let data = { 
+     "name": user.nama,  
+     "phoneNumber": user.wa,  
+     "email": user.email,  
+     "organizations": {      
+        "name": "RSUD dr. Abdul Aziz",     
+        "title": user.jab,   
+      }, 
+     "birthdays": {    
+        "day": tgl[2],     
+        "month": tgl[1],    
+        "year": tgl[0]  
+      },  
+     "bio": "NIK : "+ user.nik
+    };
+    console.log(data);
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'http://localhost:8000/api/contact/',
+  data : data
+};
+
+let post = await axios.request(config)
+console.log(post.data.data);
+await Profile.create({
+  nik: user.nik,
+  url: post.data.data.photos[0].url,
+});
+}
+
+
