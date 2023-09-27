19a1ddf3183d1d33e1ae5246fa1d96402587596b
fakhryhizballah <falehry88@gmail.com>
2023-09-27 10:15:56 +0700

    update

--- refoto.js
+++ refoto.js
@@ -8,17 +8,17 @@ async function getProfile() {
         let x = i.url;
         let newX = x.replace("s100", "s1000");
         console.log(newX);    
-        // if (startIndex !== -1) {
-        //     // Menyusun ulang URL dengan nilai "s" yang berubah
-        //     let updatedUrl = x.substring(0, startIndex + 2) + "1000";
-
-        //     console.log(updatedUrl);
-        // } else {
-        //     console.log("Parameter 's' tidak ditemukan dalam URL.");
-        // }
+     let up =   await Profile.update({
+            url: newX
+        }, {
+            where: {
+                id: i.id,
+                nik: i.nik
+            }
+        })
+        console.log(up)
+  
     }
-    
-    let x = "https://lh3.googleusercontent.com/cm/AOLgnvuUu8AcRIgeXPzrSXnRozB-rdZcp0Iw3bByggpySLnbLnAAV5XjwdvWKKImx-mY";
   
 }
 getProfile();
