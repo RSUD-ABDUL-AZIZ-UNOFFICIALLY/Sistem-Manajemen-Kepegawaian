362c5e0ec4976c59cbc625fb1f3b97503fed4e03
fakhryhizballah <falehry88@gmail.com>
2023-11-08 15:50:39 +0700

    middel ware off

--- routes/rest.js
+++ routes/rest.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const middleware = require('../middleware/rest');
 const controller = require('../controllers/rest');
 
-router.post('/getOtp', middleware.check, controller.getOtp);
-router.post('/login', middleware.check, controller.login);    
+router.post('/getOtp', controller.getOtp);
+router.post('/login', controller.login);    
 
 module.exports = router;
