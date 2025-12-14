8e3763b2654a76c2e2cacd25559837edc7618570
fakhryhizballah <falehry88@gmail.com>
2025-12-14 15:51:33 +0700

    pindah CDN Cuti

--- public/js/cuti.js
+++ public/js/cuti.js
@@ -175,7 +175,7 @@ $('#Cuti').submit(function (event) {
             var form = new FormData();
             form.append("file", file, 'surat-cuti.pdf');
             var settings = {
-                url: "https://api.rsudaa.singkawangkota.go.id/api/cdn/upload/file",
+                url: "https://api.spairum.my.id/api/cdn/upload/file",
                 method: "POST",
                 timeout: 0,
                 headers: {
@@ -202,7 +202,7 @@ $('#Cuti').submit(function (event) {
             form.append("image", file, 'surat-cuti.' + fileType);
 
             let settings = {
-                url: "https://api.rsudaa.singkawangkota.go.id/api/cdn/upload/img",
+                url: "https://api.spairum.my.id/api/cdn/upload/img",
                 method: "POST",
                 timeout: 0,
                 headers: {
