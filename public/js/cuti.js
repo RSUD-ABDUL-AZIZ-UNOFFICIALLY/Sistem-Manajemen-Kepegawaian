205a8a81ed86180450a8039b4f76679a9b3610b0
fakhryhizballah <falehry88@gmail.com>
2026-01-01 23:49:56 +0700

    update cuti 2026

--- public/js/cuti.js
+++ public/js/cuti.js
@@ -86,8 +86,8 @@ $('#jnsCuti').change(async function () {
     $('#reservation').daterangepicker({
         // opens: 'left',
         // minDate: mindate,
-        minDate: moment('2025-01-01', 'YYYY-MM-DD'),
-        maxDate: moment('2025-12-31', 'YYYY-MM-DD'),
+        minDate: moment('2026-01-01', 'YYYY-MM-DD'),
+        maxDate: moment('2026-12-31', 'YYYY-MM-DD'),
         singleDatePicker: false,
         autoApply: false,
         autoUpdateInput: true,
