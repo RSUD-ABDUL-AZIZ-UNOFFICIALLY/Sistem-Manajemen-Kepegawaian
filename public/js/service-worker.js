f5c48df56307fc3475e72b402c7fc5d63fa12663
fakhryhizballah <falehry88@gmail.com>
2026-06-22 01:11:55 +0700

    update

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const CACHE_NAME = 'simpeg-cache-v2.8.2';
-const DYNAMIC_CACHE = 'simpeg-dynamic-v2.8.2';
+const CACHE_NAME = 'simpeg-cache-v2.8.3';
+const DYNAMIC_CACHE = 'simpeg-dynamic-v2.8.3';
 console.log(CACHE_NAME, DYNAMIC_CACHE);
 
 // Daftar file statis yang harus ada di cache sejak awal (Pre-caching)
